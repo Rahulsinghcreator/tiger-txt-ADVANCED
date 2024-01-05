87053120bd9e848de0ea883c8d760b80d38600f1
kobbrak447 <153929734+kobbrak447@users.noreply.github.com>
2024-01-05 18:37:49 +0530

    Update main.pyt

--- main.py
+++ main.py
@@ -21,10 +21,9 @@ from config import *
 import sys
 import re
 import os
-
-    bot = Client(
+ bot = Client(
     "bot",
-    api_id=23621134,
+    api_id=23621134
     api_hash="3e49039179441fb424d90680ecffe365",
     bot_token="6561995810:AAGvDAcE28oD6MG39DJhMTz-4cpRuST31cM"
 
@@ -180,7 +179,7 @@ async def account_login(bot: Client, m: Message):
                     try:
                         ka = await helper.download(url, name)
                         copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        await copy.copy(chat_id = -1001074184)
+                        await copy.copy(chat_id = -1002021057745)
                         count+=1
                         os.remove(ka)
                         time.sleep(1)
@@ -194,7 +193,7 @@ async def account_login(bot: Client, m: Message):
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
                         copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
-                        await copy.copy(chat_id = -1001074184)
+                        await copy.copy(chat_id = -1002021057745)
                         count += 1
                         os.remove(f'{name}.pdf')
                     except FloodWait as e:
