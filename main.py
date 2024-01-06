8b38018f4654576f55467a59183c88d0461d1ce6
kobbrak447 <153929734+kobbrak447@users.noreply.github.com>
2024-01-06 10:43:45 +0530

    Update main.pyt

--- main.py
+++ main.py
@@ -24,10 +24,9 @@ import os
 
 bot = Client(
     "bot",
-    bot_token= "6561995810:AAGvDAcE28oD6MG39DJhMTz-4cpRuST31cM")
-    api_id=23621134,
+    api_id = 23621134 ,
     api_hash= "3e49039179441fb424d90680ecffe365"
-             
+    bot_token= "6561995810:AAGvDAcE28oD6MG39DJhMTz-4cpRuST31cM")         
              
             
 
